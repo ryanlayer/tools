64a2a67da68acc741433edf1e389561c1f97262d
Ryan Layer <ryan.layer@gmail.com>
2015-04-09 14:15:35 -0400

    catch errors

--- templates/python_template.py
+++ templates/python_template.py
@@ -4,21 +4,37 @@ import numpy as np
 
 from optparse import OptionParser
 
-parser = OptionParser()
+def prog():
+    parser = OptionParser()
 
-parser.add_option("-d",
-    "--data_file",
-    dest="data_file",
-    help="Data file")
+    parser.add_option("-d",
+        "--data_file",
+        dest="data_file",
+        help="Data file")
 
-(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-if not options.data_file:
-    parser.error('Data file not given')
+    if not options.data_file:
+        parser.error('Data file not given')
 
-f = open(options.data_file,'r')
+    f = open(options.data_file,'r')
 
-for l in f:
-    A = l.rstrip().split('\t')
+    for l in f:
+        A = l.rstrip().split('\t')
 
-f.close()
+    f.close()
+
+def main():
+    try:
+        prog();
+    except IOError as err:
+        sys.stderr.write("IOError " + str(err) + '\n');
+        return;
+    except StandardError as err:
+        sys.stderr.write("StandardError\n");
+        handleException(err);
+        sys.exit(1);
+
+if __name__ == "__main__":
+    sys.exit(main())
+    (END)
